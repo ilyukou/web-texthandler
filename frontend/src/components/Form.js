ac74d9b3cbbe97dd902d2ffc555b109950e71640
Ilya Ilyukou <ilyailyukou@gmail.com>
2019-11-17 15:17:17 +0300

    work frontend with components in center of page ( but components is lags , not refresh when state is change )

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -20,7 +20,7 @@ class Form extends Component {
         event.preventDefault();
         //const data = document.getElementById('field5').value;
         const data = new FormData(event.target);
-        fetch('http://localhost:8083/backend/upload', {
+        fetch('http://localhost:8083/backend/data', {
             mode : 'cors',
             method: 'POST',
             body: data
@@ -51,6 +51,10 @@ class Form extends Component {
     }
 
     render() {
+        var buttonGetData = <button className="btn" type="button"
+                                    onClick={this.getDataFromServlet}>Get Data</button>;
+        var buttonGetSortedData = <button className="btn btn-second" type="button"
+                                          onClick={this.getSortedDataFromServlet}>Get Sorted Data</button>;
         //  onSubmit={this.postDataToServlet}
         var form = <form onSubmit={this.postDataToServlet} >
             <ul className="form-style-1">
@@ -61,14 +65,12 @@ class Form extends Component {
                 </li>
                 <li>
                     <input type="submit" value="Upload"/>
+                    {buttonGetData}
+                    {buttonGetSortedData}
                 </li>
             </ul>
         </form>;
 
-        var buttonGetData = <button className="submit btn" type="submit"
-                                    onClick={this.getDataFromServlet}>Get Data</button>;
-        var buttonGetSortedData = <button className="submit btn" type="submit"
-                                          onClick={this.getSortedDataFromServlet}>Get Sorted Data</button>;
         // var buttonGetData = <input className="form-style-1" type="submit"
         //                             onClick={this.getDataFromServlet} value="Get Data"/>;
         // var buttonGetSortedData = <input className="form-style-1" type="submit"
@@ -77,16 +79,12 @@ class Form extends Component {
             return(
                 <div className="NewForm">
                     {form}
-                    {buttonGetData}
-                    {buttonGetSortedData}
                 </div>
             );
         }else {
             return (
                 <div className="NewForm">
                     {form}
-                    {buttonGetData}
-                    {buttonGetSortedData}
                     <Text value={this.state.json}/>
                 </div>
             );
